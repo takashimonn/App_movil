405f73683107c36b98abc7a5f541fdf055f3972a
takashimonn <takashimoon7@gmail.com>
2024-03-16 22:58:47 -0600

    registro mejorado segun yo

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
-import RegisterScreen from './RegisterScreen';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
@@ -16,21 +15,30 @@ const LoginScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bienvenid@</Text>
-      <View style={styles.cajaLogo}>
-        <Image source={require('../../assets/logo-niño-trans.png')} style={styles.logo} />
-      </View>
-
-      <View style={styles.form}>
-        <TextInput style={styles.input} placeholder="Nombre de usuario" />
-        <TextInput style={styles.input} placeholder="Contraseña" secureTextEntry />
+      <Image source={require('../../assets/granja.jpg')} style={styles.logo} />
+      <Text style={styles.texto}> Inicia Sesión </Text>
+      <View style={styles.containerBlue}>
+        <View style={styles.form}>
+          <TextInput style={styles.input} placeholder="Nombre de usuario" placeholderTextColor="#000" />
+          <TextInput style={styles.input} placeholder="Contraseña" placeholderTextColor="#000" secureTextEntry />
+        </View>
         <TouchableOpacity style={styles.button} onPress={handleLogin}>
-          <Text style={styles.buttonText}>Entrar</Text>
+          <Text style={styles.buttonText}>Continuar</Text>
         </TouchableOpacity>
 
-        <Text style={styles.registerText}>¿Aún no tienes una cuenta?</Text>
-        <TouchableOpacity onPress={handleRegister}>
-          <Text style={styles.registerLink}>Regístrate</Text>
+        <View style={styles.registerContainer}>
+          <Text style={styles.registerText}>¿Aún no tienes una cuenta?</Text>
+          <Text style={styles.link} onPress={handleRegister}>Registrarse</Text>
+        </View>
+      </View>
+      <Text style={styles.textRegistro}> O inicia sesión con tus redes sociales </Text>
+      <View style={styles.contRegistro}>
+        <TouchableOpacity style={styles.buttonRegistro} onPress={handleRegister}>
+          <Image source={require('../../assets/google.png')} style={styles.imagenRegistro}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonRegistro} onPress={handleRegister}>
+          <Image source={require('../../assets/facebook.png')} style={styles.imagenRegistro}/>
         </TouchableOpacity>
       </View>
     </View>
@@ -42,27 +50,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#FFF',
   },
-  form: {
+  containerBlue: {
+    width: '80%',
     backgroundColor: '#FFF',
     borderRadius: 10,
-    padding: 20,
-    width: 350,
-    height: 400,
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
+    paddingVertical: 30,
   },
-  title: {
-    fontSize: 20,
+  texto: {
+    fontSize: 40,
+    marginBottom: 30,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginTop: 10,
+    fontFamily: 'Pacifico', 
+  },
+  form: {
+    width: '90%',
+    marginTop: 10,
   },
   input: {
     borderWidth: 1,
@@ -70,38 +76,65 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
-    width: '90%',
-    marginTop: 20,
-    height: 60,
   },
   button: {
-    backgroundColor: 'green',
+    backgroundColor: '#5D62D5',
     padding: 10,
     borderRadius: 5,
     width: '90%',
+    height: 50,
     alignItems: 'center',
     marginTop: 20,
-    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
-    fontSize: 20,
-  },
-  registerText: {
-    marginTop: 50,
-  },
-  registerLink: {
-    color: 'blue',
-    textDecorationLine: 'underline',
-    marginTop: 15,
-  },
-  cajaLogo: {
-    marginBottom: 20,
+    fontSize: 18,
   },
   logo: {
     width: 150,
     height: 150,
+    marginBottom: 40,
+  },
+  textRegistro: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    color: '#5D62D5',
+  },
+  link: {
+    color: '#5D62D5',
+    fontSize: 18,
+    textDecorationLine: 'underline',
+  },
+  buttonRegistro: {
+    width: 100,
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#9393A7',
+    borderRadius: 5,
+    marginRight: 20, 
+    marginLeft: 20
+  },
+  imagenRegistro: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  contRegistro: {
+    flexDirection: 'row', 
+    marginTop: 40, 
+  },
+  registerContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    alignItems: 'center',
+  },
+  registerText: {
+    marginRight: 10,
+    fontSize: 16,
   },
 });
 
