0efee12752d20ce9ec629ffda9d97e098d322a2c
takashimonn <takashimoon7@gmail.com>
2024-04-27 16:04:57 -0600

    iconos de inicio de tab navigation

--- src/navigation/TabNavigation.js
+++ src/navigation/TabNavigation.js
@@ -1,4 +1,101 @@
+// import React from 'react';
+// import { Image } from 'react-native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import InicioScreen from '../screens/InicioScreen';
+// import ParametrosScreen from '../screens/ParametrosScreen';
+// import AgregarCabScreen from '../screens/AgregarCabScreen';
+// import ChequeoScreen from '../screens/ChequeoScreen';
+// import PerfilScreen from '../screens/PerfilScreen';
+// import VetScren from '../screens/VetScren';
+// import NewVetsScreen from '../screens/NewVetsScreen';
+// import BienvenidaScreen from '../screens/BienvenidaScreen';
+// import { Icon } from 'react-native-elements';
+// import { Ionicons } from '@expo/vector-icons';
+// import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+// const Tab = createBottomTabNavigator();
+// const Stack = createStackNavigator();
+
+// function InicioStack() {
+//   return (
+//     <Stack.Navigator>
+//         <Stack.Screen
+//           name="InicioStackScreen"
+//           component={InicioScreen}
+//           options={{ headerShown: false }}
+//         />
+//         <Stack.Screen name="Parametros" component={ParametrosScreen} options={{ headerShown: false }} />
+//         <Stack.Screen name="NewVets" component={NewVetsScreen} options={{ headerShown: false}}  />
+//         <Stack.Screen name="NuevoCab" component={AgregarCabScreen} options={{ headerShown: false}} />
+//         <Stack.Screen name="Chequeo" component={ChequeoScreen} options={{ headerShown: false }} />
+//         <Stack.Screen name="vets" component={VetScren} options={{ headerShown: false }} />
+//     </Stack.Navigator>
+//   );
+// }
+
+// export default function TabNavigation() {
+//   return (
+//     <Tab.Navigator>
+
+//         <Tab.Screen 
+//             name="Bienvenido" 
+//             component={BienvenidaScreen} 
+//             options={{ 
+//               headerShown: false,
+//               tabBarIcon: () => <Ionicons name="home-outline" size={24} color="black" /> 
+//                 }}  />
+
+//         <Tab.Screen 
+//             name="Inicio" 
+//             component={InicioStack} 
+//             options={{ 
+//               headerShown: false,
+//               tabBarIcon: ({ size }) => (
+//                 <Image source={require('../../assets/inicio_caballo.png')} />
+//               )}}  
+//         />
+
+//           <Tab.Screen
+//             name="Veterinarios"
+//             component={VetScren}
+//             options={({ 
+//               headerShown: false, 
+//               tabBarIcon: ({ size }) => (
+//                 <Icon
+//                   name="bag-add-outline"
+//                   type="ionicon"
+//                   size={size}
+//                   color="black"
+//                 />
+//               )})
+//             }
+//           />          
+
+//         <Tab.Screen
+//             name="Perfil"
+//             component={PerfilScreen}
+//             options={{
+//               headerShown: false,
+//               tabBarIcon: () => <Ionicons name="person-outline" size={24} color="black" />
+
+//             }}
+//         />
+//           {/* <Tab.Screen
+//             name="NewVeterinarios"
+//             component={NewVetsScreen}
+//             options={{
+//               headerShown: false, headerTitle: false,
+//               tabBarIcon: () => <Ionicons name="person-outline" size={24} color="black" />
+
+//             }}
+//         /> */}
+//     </Tab.Navigator>
+//   );
+// }
+
 import React from 'react';
+import { Image } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import InicioScreen from '../screens/InicioScreen';
@@ -9,9 +106,7 @@ import PerfilScreen from '../screens/PerfilScreen';
 import VetScren from '../screens/VetScren';
 import NewVetsScreen from '../screens/NewVetsScreen';
 import BienvenidaScreen from '../screens/BienvenidaScreen';
-import { Icon } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -36,32 +131,34 @@ function InicioStack() {
 export default function TabNavigation() {
   return (
     <Tab.Navigator>
+
         <Tab.Screen 
             name="Bienvenido" 
             component={BienvenidaScreen} 
             options={{ 
               headerShown: false,
-              tabBarIcon: () => <Ionicons name="home-outline" size={24} color="black" /> 
-                }}  />
+              tabBarIcon: () => (
+                <Image source={require('../../assets/granero.png')} style={{ width: 25, height: 25 }} />
+              )}}   
+        />
+
         <Tab.Screen 
             name="Inicio" 
             component={InicioStack} 
             options={{ 
               headerShown: false,
-              tabBarIcon: () => <MaterialCommunityIcons name="horse" size={24} color="blue" /> 
-                }}  />
+              tabBarIcon: () => (
+                <Image source={require('../../assets/inicio_caballo.png')} style={{ width: 30, height: 30 }} />
+              )}}  
+        />
+
           <Tab.Screen
             name="Veterinarios"
             component={VetScren}
             options={({ 
               headerShown: false, 
               tabBarIcon: ({ size }) => (
-                <Icon
-                  name="bag-add-outline"
-                  type="ionicon"
-                  size={size}
-                  color="black"
-                />
+                <Image source={require('../../assets/veterinario.png')} style={{ width: 25, height: 25 }} />
               )})
             }
           />          
@@ -69,22 +166,13 @@ export default function TabNavigation() {
         <Tab.Screen
             name="Perfil"
             component={PerfilScreen}
-            options={{
-              headerShown: false,
-              tabBarIcon: () => <Ionicons name="person-outline" size={24} color="black" />
-
-            }}
+            options={({ 
+              headerShown: false, 
+              tabBarIcon: ({ size }) => (
+                <Image source={require('../../assets/usuario.png')} style={{ width: 20, height: 20ad }} />
+              )})
+            }
         />
-          {/* <Tab.Screen
-            name="NewVeterinarios"
-            component={NewVetsScreen}
-            options={{
-              headerShown: false, headerTitle: false,
-              tabBarIcon: () => <Ionicons name="person-outline" size={24} color="black" />
-
-            }}
-        /> */}
     </Tab.Navigator>
   );
 }
-
