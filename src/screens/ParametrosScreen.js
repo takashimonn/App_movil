d8cd9110f99940c328c9c89ee87ddce563053ff4
takashimonn <takashimoon7@gmail.com>
2024-04-27 18:05:46 -0600

    boton back en parametros

--- src/screens/ParametrosScreen.js
+++ src/screens/ParametrosScreen.js
@@ -16,8 +16,8 @@ const ParametrosScreen = ({ route }) => {
       try {
         const response = await axios.get(`https://app-movil-lzm2.vercel.app/api/horse/${route.params.caballoId}`, {
           headers: {
-            'x-access-token': `${token}`
-        }
+            'x-access-token': token
+          }
         });
         setHorseData(response.data);
       } catch (error) {
@@ -30,11 +30,11 @@ const ParametrosScreen = ({ route }) => {
 
   const handleShowInfo = () => {
     setShowInfo(true);
-  }
+  };
 
   const handleCloseInfo = () => {
     setShowInfo(false);
-  }
+  };
 
   const showDataAlert = () => {
     Alert.alert(
@@ -50,12 +50,9 @@ const ParametrosScreen = ({ route }) => {
     );
   };
 
-
   const chequeo = () => {
     navigation.navigate('Chequeo', { caballoNombre: horseData ? horseData.name : "Nombre del Caballo" });
-
   };
-  
 
   return (
     <View style={styles.container}>
@@ -63,11 +60,15 @@ const ParametrosScreen = ({ route }) => {
         <Text style={styles.textHorseName}>
           {horseData ? horseData.name : "Cargando..."}
         </Text>
+        <TouchableOpacity style={styles.goBackButton} onPress={() => navigation.navigate('InicioStackScreen')}>
+          <Ionicons name="arrow-back" size={24} color="#21AEF9" />
+        </TouchableOpacity>
       </View>
 
-
-{/* Contenedores principal de parametros */}
+      {/* Contenedores principal de parametros */}
+      {/* Contenedores principales de parámetros */}
       <View style={styles.parametrosContainer}>
+        {/* Contenedor 1 */}
         {/* Contenedor 1 */}
         <View style={styles.vista1}>
           <Text style={styles.textvista1}>
@@ -82,6 +83,7 @@ const ParametrosScreen = ({ route }) => {
         </View>
 
         {/* Contenedor 2  */}
+        {/* Contenedor 2 */}
         <View style={styles.vista1}>
           <Text style={styles.textvista1}>
             Pulsos
@@ -94,7 +96,8 @@ const ParametrosScreen = ({ route }) => {
           </View>
         </View>
 
-        {/* Conteneodr 3 */}
+        {/* Contenedor 3 */}
+        {/* Contenedor 3 */}
         <View style={styles.vista1}>
           <Text style={styles.textvista1}>
             Velocidad
@@ -107,13 +110,14 @@ const ParametrosScreen = ({ route }) => {
           </View>
         </View>
 
-{/* Botones */}
+        {/* Botones */}
+        {/* Botones */}
         <View style={styles.contBtn}>
           <TouchableOpacity
             style={styles.btnInfo}
             onPress={handleShowInfo}
           >
-          <Text style={styles.textBtnUbicacion}>Ver Info</Text>
+            <Text style={styles.textBtnUbicacion}>Ver Info</Text>
           </TouchableOpacity>
 
           <TouchableOpacity
@@ -125,38 +129,36 @@ const ParametrosScreen = ({ route }) => {
         </View>
       </View>
 
-{/* Ventana que muestra los datos del caballo */}
+      {/* Ventana que muestra los datos del caballo */}
       {showInfo && (
-          <View style={styles.overlay}>
-            {/* <Text style={styles.textVentana}>Información de caballo</Text> */}
-            <Image 
-              source={require('../../assets/horse_alert-removebg-preview.png')} // Ajusta la ruta de la imagen
-              style={styles.horseImage} 
-              resizeMode="cover"
-            />
-            <View style={styles.infoContainer}>
-              <Text style={styles.infoText}>
-                Nombre: {horseData.name}
-              </Text>
-              <Text style={styles.infoText}>
-                Edad: {horseData.age}
-              </Text>
-              <Text style={styles.infoText}>
-                Raza: {horseData.breed}
-              </Text>
-              <Text style={styles.infoText}>
-                Enfermedades: {horseData.diseases}
-              </Text>
-              {/* <Text style={styles.infoText}>
+        <View style={styles.overlay}>
+          <Image
+            source={require('../../assets/horse_alert-removebg-preview.png')} // Ajusta la ruta de la imagen
+            style={styles.horseImage}
+            resizeMode="cover"
+          />
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoText}>
+              Nombre: {horseData.name}
+            </Text>
+            <Text style={styles.infoText}>
+              Edad: {horseData.age}
+            </Text>
+            <Text style={styles.infoText}>
+              Raza: {horseData.breed}
+            </Text>
+            <Text style={styles.infoText}>
+              Enfermedades: {horseData.diseases}
+            </Text>
+            {/* <Text style={styles.infoText}>
                 Usuario: {horseData.user}
               </Text> */}
-              <TouchableOpacity onPress={handleCloseInfo}>
-                <Text style={styles.closeText}>Cerrar</Text>
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={handleCloseInfo}>
+              <Text style={styles.closeText}>Cerrar</Text>
+            </TouchableOpacity>
           </View>
-        )}
-
+        </View>
+      )}
     </View>
   );
 }
@@ -171,16 +173,30 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 130,
     backgroundColor: '#21AEF9',
-    justifyContent: 'center',  
-    paddingLeft: '5%', 
-   
+    paddingLeft: '5%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#21AEF9',
+    paddingHorizontal: 20,
+
   },
 
   textHorseName: {
     fontSize: 30,
     fontWeight: 'bold',
-    textAlign: 'left',  
+    textAlign: 'left',
     color: 'white',
+    marginTop: '10%'
+  },
+
+  goBackButton: {
+    backgroundColor: 'white',
+    borderRadius: 50,
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: '10%'
   },
 
@@ -198,21 +214,21 @@ const styles = StyleSheet.create({
     height: '25%',
     borderRadius: 20,
     marginTop: 20,
-    elevation: 5,  
-    shadowColor: 'rgba(0, 0, 0, 0.8)',  
-    shadowOffset: { width: 0, height: 2 },  
-    shadowOpacity: 0.8,  
-    shadowRadius: 4,  
-    },
+    elevation: 5,
+    shadowColor: 'rgba(0, 0, 0, 0.8)',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+  },
 
   textvista1: {
     color: 'black',
     fontSize: 20,
     marginLeft: '7%',
-    marginBottom: 10, 
+    marginBottom: 10,
     marginTop: '5%'
   },
-  iconContainer:{
+  iconContainer: {
     justifyContent: 'center',
     marginLeft: '-5%',
     marginTop: '10%'
@@ -222,23 +238,23 @@ const styles = StyleSheet.create({
     backgroundColor: '#E2F2FB',
     width: '40%',
     height: '100%',
-    marginLeft: 'auto',  
-    alignItems: 'center', 
-    justifyContent: 'center', 
+    marginLeft: 'auto',
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 20
   },
 
   contBtn: {
     flexDirection: 'row',
-    justifyContent: 'space-between', 
+    justifyContent: 'space-between',
     marginTop: 20,
     width: '100%',
   },
 
   btnInfo: {
-    width: '40%', 
+    width: '40%',
     marginTop: '5%',
-    
+
   },
 
   textBtnUbicacion: {
@@ -248,10 +264,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#21AEF9',
     color: 'white',
     textAlign: 'center',
-    lineHeight: 50, 
+    lineHeight: 50,
     fontWeight: 'bold',
     fontSize: 18
-    
+
   },
 
   overlay: {
@@ -271,7 +287,7 @@ const styles = StyleSheet.create({
   infoText: {
     marginBottom: 10,
     fontSize: 18
-  }, 
+  },
 
   closeText: {
     marginTop: 20,
@@ -281,19 +297,11 @@ const styles = StyleSheet.create({
 
   horseImage: {
     width: '20%',
-    height: '15%', 
+    height: '15%',
     marginBottom: 20,
     borderRadius: 10,
   },
-  textVentana:{
-    color: 'white',
-    fontWeight: 'bold',
-    fontFamily: 20,
-    alignItems:'center'
-  }
-
 });
 
 export default ParametrosScreen;
-
 
